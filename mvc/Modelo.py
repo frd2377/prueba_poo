ffbd3917cb46a4147185ea81dd4d98e05212dad8
David Freddy <111796950+frd2377@users.noreply.github.com>
2023-05-23 19:23:43 -0500

    Update Modelo.py

--- mvc/Modelo.py
+++ mvc/Modelo.py
@@ -1,8 +1,17 @@
 class Modelo:
-  
-    def registrar():
-        pass  
-    def actualizar_bateria():
-        pass
-    def mostrar():
-        pass
+    sensores = []
+    baterias = []
+    def registrar(self,sensor,bateria):
+        if bateria >= 0:
+            self.sensores.append(sensor)
+            self.baterias.append(bateria)
+            
+    def actualizar_bateria(self,sensorid,bateriaid):
+        for i in range(len(self.sensores)):
+            if self.sensores[i] == sensorid:
+                self.baterias[i] = bateriaid
+
+    def mostrar(self,sensorid):
+        for i in range(len(self.sensores)):
+            if self.sensores[i] == sensorid:
+                return self.baterias[i]
